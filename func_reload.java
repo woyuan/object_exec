f37819c80fd285001a41fd6eea4b3e25f2b48937
woyuan <413424883@qq.com>
2015-09-01 20:54:05 +0800

    test
    test

---
+++ func_reload.java
@@ -0,0 +1,10 @@
+class A{
+	A(){}
+
+	void funA(){
+		System.out.println("diyige");
+	}
+	void funA(int i){
+		System.out.println("dierge");
+	}
+}
---
+++ func_reload_test.java
@@ -0,0 +1,7 @@
+class test{
+public static void main(String[] args) {
+	A a=new A();
+	a.funA();
+	a.funA(9);
+	
+}}
